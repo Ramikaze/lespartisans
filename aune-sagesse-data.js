0a843a5cad11f3e9f9eb4fa8df615c928e43d863
Ramikaze <rami.dakhlallah@gmail.com>
2026-05-16 16:52:45 +0100

    Update aune-sagesse-data.js

--- aune-sagesse-data.js
+++ aune-sagesse-data.js
@@ -685,6 +685,731 @@ const AUNE_SAGESSE = [
       },
       {
         "title": "176 - La foi et les péchés"
+      },
+      {
+        "title": "177 - Ce qui permet d'accomplir la foi"
+      },
+      {
+        "title": "178 - L'augmentation de la foi"
+      },
+      {
+        "title": "179 - Les degrés de la foi"
+      },
+      {
+        "title": "180 - Les piliers de la foi"
+      },
+      {
+        "title": "181 - Les anses les plus solides de la foi"
+      },
+      {
+        "title": "182 - La foi enracinée et passagère"
+      },
+      {
+        "title": "183 - Ce qui consolide la foi"
+      },
+      {
+        "title": "184 - Le goût de la foi"
+      },
+      {
+        "title": "185 - L'incapacité de savourer la douceur de la foi"
+      },
+      {
+        "title": "186 - Ce qui fait sortir la personne de la foi"
+      },
+      {
+        "title": "187 - Ce qui est incompatible avec la foi"
+      },
+      {
+        "title": "188 - Le haut statut du croyant"
+      },
+      {
+        "title": "189 - Les croyants sont comme un seul corps"
+      },
+      {
+        "title": "190 - Qui est le croyant ?"
+      },
+      {
+        "title": "191 - La solidité du croyant"
+      },
+      {
+        "title": "192 - La lumière du croyant"
+      },
+      {
+        "title": "193 - La rareté des [vrais] croyants"
+      },
+      {
+        "title": "194 - Les signes distinctifs du croyant"
+      },
+      {
+        "title": "195 - Le meilleur des croyants"
+      },
+      {
+        "title": "196 - Le mérite de celui qui croit au Prophète (s) sans l'avoir vu"
+      }
+    ]
+  },
+  {
+    "title": "31 - LA CONFIANCE",
+    "subparts": [
+      {
+        "title": "197 - La nécessité de respecter la confiance"
+      },
+      {
+        "title": "198 - Le caractère inconditionnel de l'obligation de respecter la confiance accordée"
+      },
+      {
+        "title": "199 - Celui qui n'est pas digne de confiance est dénué de foi"
+      },
+      {
+        "title": "200 - Les conséquences de la confiance dans la préservation des dépôts"
+      },
+      {
+        "title": "201 - Ceux à qui il est interdit de faire confiance"
+      }
+    ]
+  },
+  {
+    "title": "32 - LA SÛRETÉ",
+    "subparts": [
+      {
+        "title": "202 - La nécessité de respecter la sûreté"
+      },
+      {
+        "title": "203 - La fidélité aux pactes de sûreté"
+      },
+      {
+        "title": "204 - Le respect des pactes de sûreté"
+      }
+    ]
+  },
+  {
+    "title": "33 - LA FAMILIARITÉ",
+    "subparts": [
+      {
+        "title": "205 - Ce vis-à-vis de quoi il faut se familiariser"
+      },
+      {
+        "title": "206 - Ce vis-à-vis de quoi il faut éviter de se familiariser"
+      },
+      {
+        "title": "207 - Ce qui suscite la familiarité d'Allah"
+      }
+    ]
+  },
+  {
+    "title": "34 - L'HOMME",
+    "subparts": [
+      {
+        "title": "208 - La dignité et l'honneur des enfants d'Adam"
+      },
+      {
+        "title": "209 - La raison de la création de l'homme"
+      },
+      {
+        "title": "210 - La faiblesse de l'homme"
+      },
+      {
+        "title": "211 - Mesurer la valeur de l'être humain"
+      }
+    ]
+  },
+  {
+    "title": "35 - LES FLÉAUX",
+    "subparts": [
+      {
+        "title": "212 - Chaque chose comporte un fléau"
+      }
+    ]
+  },
+  {
+    "title": "36 - L'AVARICE",
+    "subparts": [
+      {
+        "title": "213 - Mise en garde contre l'avarice"
+      },
+      {
+        "title": "214 - L'explication de l'avarice"
+      },
+      {
+        "title": "215 - L'avare"
+      },
+      {
+        "title": "216 - Le véritable avare"
+      },
+      {
+        "title": "217 - La plus avare des personnes"
+      },
+      {
+        "title": "218 - Le signe de l'avarice"
+      }
+    ]
+  },
+  {
+    "title": "37 - L'INNOVATION (AL-BID'A)",
+    "subparts": [
+      {
+        "title": "219 - Mise en garde contre l'innovation"
+      },
+      {
+        "title": "220 - Les innovateurs"
+      },
+      {
+        "title": "221 - La signification de l'innovation"
+      },
+      {
+        "title": "222 - Le rejet de l'innovateur"
+      },
+      {
+        "title": "223 - L'innovateur et l'adoration"
+      },
+      {
+        "title": "224 - La nullité de l'acte de l'innovateur"
+      },
+      {
+        "title": "225 - Les obligations du savant lors de l'apparition des innovations"
+      }
+    ]
+  },
+  {
+    "title": "38 - LE GASPILLAGE",
+    "subparts": [
+      {
+        "title": "226 - Le blâme du gaspillage"
+      },
+      {
+        "title": "227 - La signification du gaspillage"
+      }
+    ]
+  },
+  {
+    "title": "39 - LA VERTU",
+    "subparts": [
+      {
+        "title": "228 - Incitation à la vertu"
+      },
+      {
+        "title": "229 - Le signe de la personne vertueuse"
+      },
+      {
+        "title": "230 - Le sommet de la vertu"
+      }
+    ]
+  },
+  {
+    "title": "40 - LE MONDE INTERMÉDIAIRE (AL-BARZAKH)",
+    "subparts": [
+      {
+        "title": "231 - La signification du monde intermédiaire"
+      },
+      {
+        "title": "232 - L'âme des croyants dans le monde intermédiaire"
+      },
+      {
+        "title": "233 - L'âme des mécréants dans le monde intermédiaire"
+      }
+    ]
+  },
+  {
+    "title": "41 - LA BÉNÉDICTION",
+    "subparts": [
+      {
+        "title": "234 - La signification de la bénédiction"
+      },
+      {
+        "title": "235 - Ce qui suscite la bénédiction et ce qui l'éloigne"
+      }
+    ]
+  },
+  {
+    "title": "42 - LA GAIETÉ",
+    "subparts": [
+      {
+        "title": "236 - L'encouragement à la gaieté"
+      }
+    ]
+  },
+  {
+    "title": "43 - LA CLAIRVOYANCE",
+    "subparts": [
+      {
+        "title": "237 - La clairvoyance"
+      }
+    ]
+  },
+  {
+    "title": "44 - LE FAUX",
+    "subparts": [
+      {
+        "title": "238 - Mise en garde contre le fait de suivre le faux"
+      },
+      {
+        "title": "239 - Discerner la vérité du faux"
+      },
+      {
+        "title": "240 - Mêler la vérité au faux"
+      },
+      {
+        "title": "241 - Le faux ne peut être reconnu comme étant le vrai"
+      }
+    ]
+  },
+  {
+    "title": "45 - L'AVERSION",
+    "subparts": [
+      {
+        "title": "242 - Ceux qu'Allah a en aversion"
+      },
+      {
+        "title": "243 - Les personnes qu'Allah a le plus en aversion"
+      },
+      {
+        "title": "244 - Les actes qu'Allah a en aversion"
+      },
+      {
+        "title": "245 - La malveillance"
+      }
+    ]
+  },
+  {
+    "title": "46 - L'AGRESSION",
+    "subparts": [
+      {
+        "title": "246 - Mise en garde contre l'agression"
+      },
+      {
+        "title": "247 - Les caractéristiques de l'agresseur"
+      },
+      {
+        "title": "248 - Combattre les agresseurs parmi les musulmans"
+      },
+      {
+        "title": "249 - La bénédiction de l'Imām 'Alī (as) combattant les agresseurs"
+      }
+    ]
+  },
+  {
+    "title": "47 - LES PLEURS",
+    "subparts": [
+      {
+        "title": "250 - Pleurer par crainte d'Allah"
+      },
+      {
+        "title": "251 - La sécheresse de l'œil"
+      }
+    ]
+  },
+  {
+    "title": "48 - LA CONTRÉE",
+    "subparts": [
+      {
+        "title": "252 - La bonne contrée"
+      },
+      {
+        "title": "253 - Choisissez les grandes villes"
+      },
+      {
+        "title": "254 - La meilleure des contrées"
+      },
+      {
+        "title": "255 - Ce dont ne peuvent se passer les habitants de toute contrée"
+      }
+    ]
+  },
+  {
+    "title": "49 - L'ÉLOQUENCE",
+    "subparts": [
+      {
+        "title": "256 - La signification de l'éloquence"
+      },
+      {
+        "title": "257 - La plus éloquente des paroles"
+      },
+      {
+        "title": "258 - La manipulation par la parole"
+      }
+    ]
+  },
+  {
+    "title": "50 - LA DIFFUSION [DE L'ISLAM]",
+    "subparts": [
+      {
+        "title": "259 - L'importance de la diffusion [de l'islam]"
+      },
+      {
+        "title": "260 - Ce qui incombe au diffuseur [de l'islam]"
+      }
+    ]
+  },
+  {
+    "title": "51 - L'ÉPREUVE",
+    "subparts": [
+      {
+        "title": "261 - Être éprouvé par le mal et le bien"
+      },
+      {
+        "title": "262 - La philosophie de l'épreuve"
+      },
+      {
+        "title": "263 - La dureté des épreuves du croyant"
+      },
+      {
+        "title": "264 - Le rôle des mauvais actes dans l'apparition des épreuves"
+      },
+      {
+        "title": "265 - Celui qui n'est pas éprouvé est méprisé par Allah"
+      },
+      {
+        "title": "266 - La grâce de l'épreuve"
+      },
+      {
+        "title": "267 - L'épreuve et le rappel"
+      },
+      {
+        "title": "268 - Les péchés sont épurés par les épreuves"
+      },
+      {
+        "title": "269 - L'épreuve est un signe de l'amour d'Allah, loué soit-Il"
+      },
+      {
+        "title": "270 - L'épreuve est proportionnelle au degré de la foi"
+      },
+      {
+        "title": "271 - Les degrés auxquels accède le serviteur par les épreuves"
+      },
+      {
+        "title": "272 - L'épreuve du croyant est un bien pour lui"
+      },
+      {
+        "title": "273 - L'épreuve la plus difficile des serviteurs"
+      },
+      {
+        "title": "274 - La délivrance au sommet de l'épreuve"
+      },
+      {
+        "title": "275 - Le rappel d'Allah lors des épreuves"
+      },
+      {
+        "title": "276 - L'invocation à la vue de personnes éprouvées"
+      }
+    ]
+  },
+  {
+    "title": "52 - LA CALOMNIE",
+    "subparts": [
+      {
+        "title": "277 - Mise en garde contre la calomnie"
+      }
+    ]
+  },
+  {
+    "title": "53 - L'EXÉCRATION RÉCIPROQUE (AL-MUBĀHALA)",
+    "subparts": [
+      {
+        "title": "278 - L'exécration réciproque"
+      }
+    ]
+  },
+  {
+    "title": "54 - LE SERMENT D'ALLÉGEANCE (AL-BAY'A)",
+    "subparts": [
+      {
+        "title": "279 - Le serment d'allégeance au Prophète (s) est une allégeance à Allah"
+      },
+      {
+        "title": "280 - Le serment d'allégeance des femmes"
+      },
+      {
+        "title": "281 - Violer son serment d'allégeance"
+      },
+      {
+        "title": "282 - Le serment d'allégeance des musulmans à l'Imām 'Alī (as)"
+      }
+    ]
+  },
+  {
+    "title": "55 - LE COMMERCE",
+    "subparts": [
+      {
+        "title": "283 - La vertu du commerce"
+      },
+      {
+        "title": "284 - Délaisser le commerce"
+      },
+      {
+        "title": "285 - Le bon comportement dans le commerce"
+      },
+      {
+        "title": "286 - L'interdiction de la fraude"
+      },
+      {
+        "title": "287 - L'incitation du commerçant à la charité"
+      },
+      {
+        "title": "288 - L'indulgence dans la vente et l'achat"
+      },
+      {
+        "title": "289 - Le marchandage"
+      },
+      {
+        "title": "290 - L'égalité entre la personne qui marchande et celle qui ne marchande pas"
+      },
+      {
+        "title": "291 - Le profit du croyant vis-à-vis d'un autre croyant"
+      },
+      {
+        "title": "292 - Les commerçants immoraux"
+      },
+      {
+        "title": "293 - Inciter les commerçants à être honnête"
+      },
+      {
+        "title": "294 - Mise en garde au sujet du recours au serment dans le commerce"
+      },
+      {
+        "title": "295 - Le commerce de l'Au-delà"
+      },
+      {
+        "title": "296 - Le commerce ne distrait pas le croyant [du souvenir d'Allah]"
+      },
+      {
+        "title": "297 - Le commerce de la religion"
+      }
+    ]
+  },
+  {
+    "title": "56 - LE REPENTIR",
+    "subparts": [
+      {
+        "title": "298 - L'incitation au repentir"
+      },
+      {
+        "title": "299 - Le statut du repentant"
+      },
+      {
+        "title": "300 - Le signe du repentant"
+      },
+      {
+        "title": "301 - L'acceptation du repentir"
+      },
+      {
+        "title": "302 - Quand le repentir est-il accepté ?"
+      },
+      {
+        "title": "303 - Le regret est un repentir"
+      },
+      {
+        "title": "304 - L'aveu sincère [des péchés]"
+      },
+      {
+        "title": "305 - Les fondements du repentir"
+      },
+      {
+        "title": "306 - Les différentes sortes de repentir"
+      },
+      {
+        "title": "307 - Le repentir sincère"
+      },
+      {
+        "title": "308 - Retarder le repentir"
+      },
+      {
+        "title": "309 - Ce qui est plus aisé que le repentir"
+      },
+      {
+        "title": "310 - Allah dissimule les péchés du repentant"
+      },
+      {
+        "title": "311 - Changer les péchés en bonnes actions"
+      },
+      {
+        "title": "312 - Chercher à prédire [les jugements] d'Allah"
+      }
+    ]
+  },
+  {
+    "title": "57 - LA RÉCOMPENSE",
+    "subparts": [
+      {
+        "title": "313 - La récompense des bonnes œuvres"
+      },
+      {
+        "title": "314 - La plus grande récompense"
+      },
+      {
+        "title": "315 - Celui qui entend parler d'une récompense pour un acte particulier"
+      }
+    ]
+  },
+  {
+    "title": "58 - LA RÉVOLUTION",
+    "subparts": [
+      {
+        "title": "316 - La révolution islamique en Orient avant la venue du Douzième Imām (Al-Qā'im) ('aj)"
+      },
+      {
+        "title": "317 - Le rôle des non-arabes dans la révolution"
+      },
+      {
+        "title": "318 - La révolution provenant de la ville de Qom"
+      }
+    ]
+  },
+  {
+    "title": "59 - LE DÉTERMINISME",
+    "subparts": [
+      {
+        "title": "319 - La fausseté du déterminisme"
+      },
+      {
+        "title": "320 - Ni déterminisme ni liberté totale"
+      },
+      {
+        "title": "321 - A Allah revient en premier lieu le mérite des bonnes actions"
+      },
+      {
+        "title": "322 - La façon dont il faut se comporter envers les partisans du déterminisme"
+      }
+    ]
+  },
+  {
+    "title": "60 - LE TYRAN",
+    "subparts": [
+      {
+        "title": "323 - Le blâme de la tyrannie et du caractère tyrannique"
+      },
+      {
+        "title": "324 - La mauvaise fin des tyrans"
+      }
+    ]
+  },
+  {
+    "title": "61 - LA LÂCHETÉ",
+    "subparts": [
+      {
+        "title": "325 - Le blâme de la lâcheté"
+      },
+      {
+        "title": "326 - Le lâche et la guerre"
+      }
+    ]
+  },
+  {
+    "title": "62 - LA DISCUSSION",
+    "subparts": [
+      {
+        "title": "327 - La discussion blâmée"
+      },
+      {
+        "title": "328 - La bonne discussion"
+      }
+    ]
+  },
+  {
+    "title": "63 - L'EXPÉRIENCE",
+    "subparts": [
+      {
+        "title": "329 - Les bénédictions de l'expérience"
+      },
+      {
+        "title": "330 - Les dommages issus du manque d'expérience"
+      }
+    ]
+  },
+  {
+    "title": "64 - L'ANXIÉTÉ",
+    "subparts": [
+      {
+        "title": "331 - Mise en garde contre l'anxiété"
+      }
+    ]
+  },
+  {
+    "title": "65 - LA RÉTRIBUTION",
+    "subparts": [
+      {
+        "title": "332 - La rétribution [des actes]"
+      },
+      {
+        "title": "333 - La rétribution des bienfaisants"
+      },
+      {
+        "title": "334 - La rétribution des malfaiteurs"
+      }
+    ]
+  },
+  {
+    "title": "66 - L'ESPIONNAGE",
+    "subparts": [
+      {
+        "title": "335 - L'interdiction de rechercher les défauts des gens"
+      },
+      {
+        "title": "336 - Les conséquences de l'espionnage des gens"
+      },
+      {
+        "title": "337 - La licéité de l'espionnage pendant les guerres"
+      },
+      {
+        "title": "338 - Les cas où il faut juger selon ce qui est apparent"
+      }
+    ]
+  },
+  {
+    "title": "67 - L'ASSEMBLÉE",
+    "subparts": [
+      {
+        "title": "339 - La plus honorable des assemblées"
+      },
+      {
+        "title": "340 - Ce qu'il faut respecter lors des assemblées"
+      },
+      {
+        "title": "341 - Le centre de l'assemblée"
+      },
+      {
+        "title": "342 - Les assemblées interdites"
+      },
+      {
+        "title": "343 - Respecter la loyauté dans les assemblées"
+      },
+      {
+        "title": "344 - L'incitation à assister aux assemblées où l'on pratique le rappel d'Allah (dhikr)"
+      },
+      {
+        "title": "345 - L'incitation à évoquer Allah en quittant une assemblée"
+      }
+    ]
+  },
+  {
+    "title": "68 - LA COMPAGNIE",
+    "subparts": [
+      {
+        "title": "346 - A qui tenir compagnie ?"
+      },
+      {
+        "title": "347 - Le droit du compagnon"
+      },
+      {
+        "title": "348 - Celui à qui il ne faut pas tenir compagnie"
+      }
+    ]
+  },
+  {
+    "title": "69 - LE GROUPE",
+    "subparts": [
+      {
+        "title": "349 - La main d'Allah est avec le groupe"
+      },
+      {
+        "title": "350 - La signification du groupe"
+      }
+    ]
+  },
+  {
+    "title": "70 - LE VENDREDI",
+    "subparts": [
+      {
+        "title": "351 - Le jour du vendredi"
       }
     ]
   }
